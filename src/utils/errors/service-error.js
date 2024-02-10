531df90a6cd7109a10ade46acb24aef22ceafafb
tourist-17 <yadsak00@gmail.com>
2024-02-10 15:30:28 +0530

    Added error files

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require("http-status-code");
+
+class ServiceError extends Error {
+  constructor(
+    message,
+    explanation,
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    super();
+    this.name = "ServiceError";
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ServiceError;
